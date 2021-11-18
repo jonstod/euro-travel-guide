147bfa4db60f33f33e49d5bf8478be72b473c823
Jonathon Stoddart <stoddart.jc@outlook.com>
2021-11-17 22:22:36 -0500

    Microservice implementations fixed

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from countries import countries
 import requests
 import json
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 # Configuration
 
@@ -27,24 +28,55 @@ def country(country):
     currency = data_dict['currency']
 
     # Subreddit scraper microservice implementation
-    r3 = requests.get('http://flip3.engr.oregonstate.edu:6069/sub?subreddit=%s' %country)
-    soup = BeautifulSoup(r3.text, 'html.parser')
-    
-    reddit_table = soup.find('table', class_ = 'dataframe')
+    if country == "Bosnia and Herzegovina":
+        sub = "bih"
+    elif country == "Czech Republic":
+        sub = "czech"
+    elif country == "Estonia":
+        sub = "Eesti"
+    elif country == "Georgia":
+        sub = "Sakartvelo"
+    elif country == "North Macedonia":
+        sub = "mkd"
+    elif country == "San Marino":
+        sub = "San_Marino"
+    elif country == "United Kingdom":
+        sub = "unitedkingdom"
+    elif country == "Vatican City":
+        sub = "vatican"
+    else:
+        sub = country
+
     posts = {}
-    
-    for post in reddit_table.find_all('tbody'):
-        rows = post.find_all('tr')
-        for row in rows:
-            posts[row.find('td').text.strip()] = row.find_all('td')[1].text.strip()
+
+    url = 'http://flip3.engr.oregonstate.edu:6069/sub?subreddit=%s' %sub
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+    tbody = soup.find('tbody')
+    rows = tbody.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        posts[cols[0].text.strip()] = cols[1].text.strip()
 
     titles = posts.keys()
     content = posts.values()
 
     # Currency converter microservice implementation
+    euro_countries = ['Austria', 'Belgium', 'Cyprus', 'Estonia', 'Finland', 'Greece', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 
+    'Portugal', 'Slovakia', 'Slovenia', 'Spain']
 
+    if country in ['Denmark', 'Norway', 'Sweden', 'Switzerland', 'United Kingdom']:
+        r4 = requests.get('http://flip3.engr.oregonstate.edu:4241/?%s=1' %country)
+        r4 = json.loads(r4.content)
+        conv_rate = str(r4["amount"]) + " US Dollars."
+    elif country in euro_countries:
+        r4 = requests.get('http://flip3.engr.oregonstate.edu:4241/?EMU%20Members=1')
+        r4 = json.loads(r4.content)
+        conv_rate = str(r4["amount"]) + " US Dollars."
+    else:
+        conv_rate = "Conversion unavailable"
 
-    return render_template("country.html", country=country, wiki=wiki_info, posts=posts, currency=currency)
+    return render_template("country.html", country=country, wiki=wiki_info, posts=posts, sub=sub, currency=currency, conv_rate = conv_rate)
 
 # Listener
 if __name__ == '__main__':
