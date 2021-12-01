1397558cad1a8917bb492b4f107874782df19fac
Jonathon Stoddart <stoddart.jc@outlook.com>
2021-12-01 02:12:44 -0500

    error handling, flag code dict added

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template
-from flask_cors import CORS
-from countries import countries
+from countries import countries, flags
 import requests
 import json
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 # Configuration
-
 app = Flask(__name__)
-CORS(app)
 
 # Routes
 @app.route("/")
@@ -18,65 +15,86 @@ def home():
 
 @app.route("/<country>")
 def country(country):
-    # Wikipedia scraper microservice implementation
-    r1 = requests.get('http://flip3.engr.oregonstate.edu:6231/?article=%s' %country)
-    info_dict = json.loads(r1.content)
-    wiki_info = info_dict['info']
+    # Prevention from using other parameters (other words/phrases with associated Wikipedia pages and subreddits)
+    if country not in countries:
+        return render_template("error.html", service = "URL parameters. The parameter(s) specified is not recognized as a European country.")
 
-    r2 = requests.get('http://flip3.engr.oregonstate.edu:6231/?article=%s&country_data=y' %country)
-    data_dict = json.loads(r2.content)
-    currency = data_dict['currency']
+    flag_code = flags[country]
+
+    # Wikipedia scraper microservice implementation
+    try:
+        if country == "Georgia":
+            wiki_link = "Georgia_(country)"
+        else:
+            wiki_link = country
+        r1 = requests.get('http://flip3.engr.oregonstate.edu:6231/?article=%s' %wiki_link)
+        info_dict = json.loads(r1.content)
+        wiki_info = info_dict['info']
+
+        r2 = requests.get('http://flip3.engr.oregonstate.edu:6231/?article=%s&country_data=y' %wiki_link)
+        data_dict = json.loads(r2.content)
+        currency = data_dict['currency']
+    except:
+        return render_template("error.html", service="Wikipedia scraper service")
 
     # Subreddit scraper microservice implementation
-    if country == "Bosnia and Herzegovina":
-        sub = "bih"
-    elif country == "Czech Republic":
-        sub = "czech"
-    elif country == "Estonia":
-        sub = "Eesti"
-    elif country == "Georgia":
-        sub = "Sakartvelo"
-    elif country == "North Macedonia":
-        sub = "mkd"
-    elif country == "San Marino":
-        sub = "San_Marino"
-    elif country == "United Kingdom":
-        sub = "unitedkingdom"
-    elif country == "Vatican City":
-        sub = "vatican"
-    else:
-        sub = country
+    try: 
+        if country == "Bosnia and Herzegovina":
+            sub = "bih"
+        elif country == "Czech Republic":
+            sub = "czech"
+        elif country == "Estonia":
+            sub = "Eesti"
+        elif country == "Georgia":
+            sub = "Sakartvelo"
+        elif country == "North Macedonia":
+            sub = "mkd"
+        elif country == "San Marino":
+            sub = "San_Marino"
+        elif country == "United Kingdom":
+            sub = "unitedkingdom"
+        elif country == "Vatican City":
+            sub = "vatican"
+        else:
+            sub = country
 
-    posts = {}
+        posts = {}
 
-    url = 'http://flip3.engr.oregonstate.edu:6069/sub?subreddit=%s' %sub
-    html = urlopen(url).read()
-    soup = BeautifulSoup(html, "html.parser")
-    tbody = soup.find('tbody')
-    rows = tbody.find_all('tr')
-    for row in rows:
-        cols = row.find_all('td')
-        posts[cols[0].text.strip()] = cols[1].text.strip()
+        url = 'http://flip3.engr.oregonstate.edu:6069/sub?subreddit=%s' %sub
+        html = urlopen(url).read()
+        soup = BeautifulSoup(html, "html.parser")
+        tbody = soup.find('tbody')
+        rows = tbody.find_all('tr')
+        for row in rows:
+            cols = row.find_all('td')
+            posts[cols[0].text.strip()] = cols[1].text.strip()
 
-    titles = posts.keys()
-    content = posts.values()
+        titles = posts.keys()
+        content = posts.values()
+    except:
+        return render_template("error.html", service="Subreddit scraper service")
 
     # Currency converter microservice implementation
-    euro_countries = ['Austria', 'Belgium', 'Cyprus', 'Estonia', 'Finland', 'Greece', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 
-    'Portugal', 'Slovakia', 'Slovenia', 'Spain']
+    try:
+        euro_countries = ['Austria', 'Belgium', 'Cyprus', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 
+        'Malta', 'Netherlands', 'Portugal', 'Slovakia', 'Slovenia', 'Spain']
 
-    if country in ['Denmark', 'Norway', 'Sweden', 'Switzerland', 'United Kingdom']:
-        r4 = requests.get('http://flip3.engr.oregonstate.edu:4241/?%s=1' %country)
-        r4 = json.loads(r4.content)
-        conv_rate = str(r4["amount"]) + " US Dollars."
-    elif country in euro_countries:
-        r4 = requests.get('http://flip3.engr.oregonstate.edu:4241/?EMU%20Members=1')
-        r4 = json.loads(r4.content)
-        conv_rate = str(r4["amount"]) + " US Dollars."
-    else:
-        conv_rate = "Conversion unavailable"
+        if country in ['Denmark', 'Norway', 'Sweden', 'Switzerland', 'United Kingdom']:
+            r4 = requests.get('http://flip3.engr.oregonstate.edu:4241/?%s=1' %country)
+            r4 = json.loads(r4.content)
+            conv_rate = str(r4["amount"]) + " US Dollars."
+        elif country in euro_countries:
+            r4 = requests.get('http://flip3.engr.oregonstate.edu:4241/?EMU%20Members=1')
+            r4 = json.loads(r4.content)
+            conv_rate = str(r4["amount"]) + " US Dollars."
+            currency = "Euro (€) (EUR)"
+        else:
+            conv_rate = "Conversion unavailable"
+    except:
+        return render_template("error.html", service="currency conversion")
 
-    return render_template("country.html", country=country, wiki=wiki_info, posts=posts, sub=sub, currency=currency, conv_rate = conv_rate)
+    return render_template("country.html", country=country, wiki=wiki_info, posts=posts, sub=sub, currency=currency, conv_rate = conv_rate, flag = flag_code)
+    
 
 # Listener
 if __name__ == '__main__':
